735a3949103238941534549b06fe776ebd9316d8
celenajasmin-tg <rcdjasmin@yahoo.com>
2019-09-28 12:12:00 +0800

    Updates

---
+++ precache-manifest.39296ed4054eade182f50e242ff538d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916c6362a62f18282344464236d9380b",
+    "url": "/techladies-assignment/index.html"
+  },
+  {
+    "revision": "b42cb582d1dc5f4393ea",
+    "url": "/techladies-assignment/static/css/main.4f43829d.chunk.css"
+  },
+  {
+    "revision": "9e647a8682cace9b6651",
+    "url": "/techladies-assignment/static/js/2.4a3d804d.chunk.js"
+  },
+  {
+    "revision": "b42cb582d1dc5f4393ea",
+    "url": "/techladies-assignment/static/js/main.3ea19ecf.chunk.js"
+  },
+  {
+    "revision": "969281b3fb28e1e5fd24",
+    "url": "/techladies-assignment/static/js/runtime~main.a820456c.js"
+  },
+  {
+    "revision": "c705e4de777c309febeece73305313e0",
+    "url": "/techladies-assignment/static/media/BluuNext-Bold.c705e4de.woff"
+  },
+  {
+    "revision": "0d248fece6f093412c3d55c9712c522b",
+    "url": "/techladies-assignment/static/media/Roboto-Regular.0d248fec.woff"
+  },
+  {
+    "revision": "fe3b302ff440eef113b8796df3decc14",
+    "url": "/techladies-assignment/static/media/it me.fe3b302f.JPG"
+  },
+  {
+    "revision": "b2718df0d00f753ef018dbb435dd7859",
+    "url": "/techladies-assignment/static/media/sun-moon-1.b2718df0.svg"
+  }
+]);
